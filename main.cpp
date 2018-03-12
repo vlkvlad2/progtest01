b1687f1821393ef4d97a25a0d0b0bb36d048a0a7
vlkvlad2 <vladim.vlk@centrum.cz>
2018-03-12 09:17:52 +0100

    pondeli 09:17

--- main.cpp
+++ main.cpp
@@ -30,13 +30,17 @@ bool readHead ( const char * srcFileName,
                 int16_t & width,
                 int16_t & channel,
                 int16_t & bytesPerChannel,
-                int16_t & interleaveBegin )
+                int16_t & interleaveBegin,
+                int16_t & size,
+                int16_t & final,
+                int & interleave)
 {
     ifstream from( srcFileName, ios::in | ios::binary);
     int16_t temp = 0;
     int16_t tmpChannel = 0;
     int16_t interleaveBegintmp = 0;
     int16_t head[8];
+    int16_t tmpFinal, first, second, third;
     int tmp[] = { 0, 2, 3, 4, 5};
 
     if(!from.good())
@@ -58,6 +62,7 @@ bool readHead ( const char * srcFileName,
     width = width + head[2];
     height = height + head[4];
 
+    tmpFinal = head[6];
     tmpChannel = head[6] & 3;
 
     switch (tmpChannel) {
@@ -98,7 +103,7 @@ bool readHead ( const char * srcFileName,
         return false;
 
     interleaveBegintmp = head[6] >> 5;
-    interleaveBegintmp = interleaveBegintmp & 7; 
+    interleaveBegintmp = interleaveBegintmp & 7;
     interleaveBegin = pow(2,interleaveBegintmp);
 
     if(interleaveBegin == 128)
@@ -110,25 +115,177 @@ bool readHead ( const char * srcFileName,
 
     cout << width << " " << height << " " << bytesPerChannel << " " << channel << " " << interleaveBegin  << endl;
 
+    from.seekg(0,ios_base::end);
+    size = from.tellg();
+
+    size = size - 8;
+    if (size != (height * channel * width))
+        return false;
+
+
+    final = tmpFinal | 224;
+
+    first = interleave << 5;
+
+    second = interleave & 2;
+    second = second >> 1;
+
+    cout << first << " " << second << endl;
+
+
+    cout << final << endl;
+    from.close();
+
     return true;
 
 }
 
+
+bool readPicture (const char * srcFileName,
+                  int16_t & height,
+                  int16_t & width,
+                  int16_t ** picture,
+                  bool ** isFilled,
+                  int16_t & channel,
+                  int16_t & interleaveBegin)
+{
+    ifstream from(srcFileName, ios::in | ios::binary);
+    int16_t tmp = 0;
+    from.seekg(8, ios::beg);
+
+    if (!from.good())
+        return false;
+
+   for (int16_t k = interleaveBegin; k > 0; k = k / 2)
+    {
+        for (int16_t i = 0; i < height; i = i + k)
+        {
+            for (int16_t j = 0; j < width * channel; j = j + k) {
+
+                 if (isFilled[i][j] != true) {
+                     if (!from.read((char *) &tmp, 1))
+                         return false;
+                     picture[i][j] = tmp;
+                     isFilled[i][j] = true;
+                 }
+            }
+        }
+
+    }
+
+    for (int16_t i = 0; i < height; i++)
+    {
+        for (int16_t j = 0; j < width * channel; j++)
+        {
+            cout << picture [i][j] << " ";
+
+        }
+        cout << endl;
+    }
+    from.close();
+    return true;
+
+}
+
+bool interleavePicture (const char * dstFileName,
+                        int16_t & height,
+                        int16_t & width,
+                        int16_t & channel,
+                        int16_t ** picture,
+                        int & interleave,
+                        bool ** isFilled)
+{
+    int zero = 0;
+    ofstream of;
+    of.open (dstFileName, ios::binary);
+    if (! of.good())
+        return false;
+
+    of.write((char*)&ENDIAN_LITTLE, 2);
+    of.write((char*)&height, 2);
+    of.write((char*)&width, 2);
+    of.write((char*)&zero, 1);
+    of.write((char*)&zero, 1);
+
+
+    for (int16_t k = interleave; k > 0; k = k / 2)
+    {
+        for (int16_t i = 0; i < height; i = i + k)
+        {
+            for (int16_t j = 0; j < width * channel; j = j + k) {
+                if (isFilled[i][j] != true) {
+                    if (! of.write((char*)&picture[i][j], 1))
+                        return false;
+                    isFilled[i][j] = true;
+                }
+            }
+        }
+    }
+
+    of.close();
+    return true;
+}
 
 bool recodeImage (const char  * srcFileName,
                   const char  * dstFileName,
                   int           interleave,
                   uint16_t      byteOrder)
 {
-    int16_t height, width, channel, bytesPerChannel, interleaveBegin;
-    readHead (srcFileName, height, width, channel, bytesPerChannel, interleaveBegin);
+    int16_t height = 0, width = 0, channel = 0, bytesPerChannel = 0, interleaveBegin = 0, size = 0;
+    int16_t ** picture;
+    bool ** isFilled;
+    int16_t final = 0;
+
+    readHead (srcFileName, height, width, channel, bytesPerChannel, interleaveBegin, size, final, interleave);
+
+    picture = new int16_t*[height];
+    for (int i = 0; i<height; i++)
+    {
+        picture[i] = new int16_t [width * channel];
+        for (int j = 0; j < width * channel; j++)
+        {
+            picture [i][j] = 0;
+        }
+    }
+
+    isFilled = new bool*[height];
+    for (int i = 0; i<height; i++)
+    {
+        isFilled[i] = new bool[width * channel];
+        for (int j = 0; j < width * channel; j++)
+        {
+            isFilled [i][j] = false;
+        }
+    }
+
+    readPicture (srcFileName, height, width, picture, isFilled, channel, interleaveBegin);
+
+    for (int i = 0; i<height; i++)
+    {
+        for (int j = 0; j < width * channel; j++) {
+            isFilled[i][j] = false;
+        }
+    }
+
+
+    interleavePicture (dstFileName, height, width, channel, picture, interleave, isFilled);
+
+    for (int i = 0; i < height; i++)
+    {
+        delete [] picture[i];
+
+    }
+    delete [] picture;
+
+
+
     return true;
 
 }
 
 int main() {
 
-    recodeImage("pr01/ref_01.img", "aaa", 0, 0);
+    recodeImage("pr01/input_03.img", "aaa", 2, 0);
 
     return 0;
 }
